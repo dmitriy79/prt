1792f2b832dca406b4de666c613293e0d851e37f
Your Name <sinoptik79@inbox.ru>
2019-04-06 06:18:17 +1100

    R

---
+++ depends/packages/packages.mk
@@ -0,0 +1,22 @@
+packages:=boost openssl libevent zeromq
+linux_packages_usb := libusb
+
+qt_native_packages = native_protobuf
+qt_packages = qrencode protobuf zlib
+
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+
+qt_darwin_packages=qt
+qt_mingw32_packages=qt
+
+wallet_packages=bdb
+
+upnp_packages=miniupnpc
+usb_packages=hidapi protobuf
+usb_native_packages=native_protobuf
+
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
+
+ifneq ($(build_os),darwin)
+darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+endif
---
+++ src/wallet/test/hdwallet_test_fixture.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2017-2018 The Particl Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PARTICL_WALLET_TEST_HDWALLET_TEST_FIXTURE_H
+#define PARTICL_WALLET_TEST_HDWALLET_TEST_FIXTURE_H
+
+#include <test/test_bitcoin.h>
+
+class CHDWallet;
+
+/** Testing setup and teardown for wallet.
+ */
+struct HDWalletTestingSetup: public TestingSetup {
+    HDWalletTestingSetup(const std::string& chainName = CBaseChainParams::MAIN);
+    ~HDWalletTestingSetup();
+
+    std::shared_ptr<CHDWallet> pwalletMain;
+};
+
+std::string StripQuotes(std::string s);
+
+#endif // PARTICL_WALLET_TEST_HDWALLET_TEST_FIXTURE_H
+
